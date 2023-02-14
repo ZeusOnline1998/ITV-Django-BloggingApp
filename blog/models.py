682c67501fe8eaa3438d7891a24725ce67d05afc
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-02-14 18:56:56 +0530

    BlogPost models created and registered

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,14 @@ from django.utils import timezone
 
 # Create your models here.
 class BlogPost(models.Model):
+
+    def __str__(self):
+        return self.title
+
+
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     create_date = models.DateTimeField(default=timezone.now)
     publish_date = models.DateTimeField(blank=True, null=True)
+
