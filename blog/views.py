95c7dd18a3fbdb79dd4870b8b738176700439bd4
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-02-15 18:39:43 +0530

    Dynamic content for posts in blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import BlogPost
 
 # Create your views here.
+
+def index(request):
+
+    result = BlogPost.objects.all().order_by('-id')
+    content = {
+        'blogs' : result,
+    }
+    
+    return render(request, 'blog/index.html', context=content)
