741528eb24f2814e25b3ea4273c67e19b9364ace
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-02-20 19:06:52 +0530

    post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('post_detail/<int:id>/', views.post_detail_view, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import BlogPost
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -11,3 +12,17 @@ def index(request):
     }
     
     return render(request, 'blog/index.html', context=content)
+
+def post_detail_view(request, id):
+    # try:
+        
+    #     data = BlogPost.objects.get(id=id)  ## get method to retrieve information 
+    # except ObjectDoesNotExist:              ## preferably to use whenever server requests information through methods
+    #     print("Object Not Found Error")
+
+    data = get_object_or_404(BlogPost, id=id)   ## preferably use when end user request the information
+
+    content = {
+        'post' : data
+    }
+    return render(request, 'blog/post_detail.html', context=content)
