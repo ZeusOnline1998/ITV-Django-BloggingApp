86e1e60fc17f032f25ef68432b431678e8220037
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-03-13 18:55:21 +0530

    Customise admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import BlogPost
+from django.contrib.auth.models import Group, User
 
 # Register your models here.
 admin.site.register(BlogPost)
+
+#Unregsiter
+admin.site.unregister(Group)
+admin.site.unregister(User)
+# admin.site.unregister(BlogPost)
+
+admin.site.site_header = 'Blogging Admin Panel'
+admin.site.site_title = 'Admin Panel'
+admin.site.index_title = 'Site admin'
+
