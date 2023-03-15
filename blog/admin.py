d1ee5f4f515e32dda4dd4171778a36f9fef1cf55
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-03-15 17:15:14 +0530

    Customise Admin Panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 from .models import BlogPost
 from django.contrib.auth.models import Group, User
+from django.utils.html import format_html
+
+class BlogPostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'content_colored', 'create_date') #Display the fields in the admin panel
+    list_filter = ('create_date', 'publish_date') #Add filter options
+    search_fields = ('content',) #Create a search bar based on the field provided if more field provided then it will provide only one search with multiple field search
+    list_per_page = 5 #Pagination
+    date_hierarchy = 'create_date' #Create a heirarchical structure to filter data
+    list_display_links = ('content_colored',) #Provides the link to edit the data implicitly
+    list_editable = ('title',) #Can edit the field in the base panel
+
+    def content_colored(self, obj):  #Format the html of the given column
+        return format_html(f'<span style="color: red">{obj.content}</span>')
+
+    
+
+
+    
+
 
 # Register your models here.
-admin.site.register(BlogPost)
+admin.site.register(BlogPost, BlogPostAdmin)
 
 #Unregsiter
 admin.site.unregister(Group)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.utils.html import format_html
 
 # Create your models here.
 class BlogPost(models.Model):
@@ -15,3 +16,6 @@ class BlogPost(models.Model):
     create_date = models.DateTimeField(default=timezone.now)
     publish_date = models.DateTimeField(blank=True, null=True)
 
+
+    # def content_colored(self): #Format the html of the given column in models.py
+    #     return format_html(f'<span style="color: red">{self.content}</span>')
